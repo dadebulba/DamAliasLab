36c46de2207061448f536600b32e88f076f7fb44
Giaco131 <giaco131.ktm@gmail.com>
2020-11-11 13:44:16 +0100

    Creazione classe Pedina #12

---
+++ back-end/VivaLaDama/VivaLaDama/Models/Pedina.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VivaLaDama.Models
+{
+    public class Pedina
+    {
+        public enum ColorePedina { WHITE, BLACK, NONE }
+        public long Id { get; }
+        public ColorePedina Color { get; }
+        public bool Upgraded { get; set; }
+        public Pedina(ColorePedina color, long id)
+        {
+            this.Color = color;
+            this.Id = id;
+            this.Upgraded = false;
+        }
+        public ColorePedina GetColoreOpponente()
+        {
+            if(Color==ColorePedina.NONE)
+            {
+                return Color;
+            }
+            return (Color == ColorePedina.WHITE ? ColorePedina.BLACK : ColorePedina.WHITE);
+        }
+        public override bool Equals(Object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            Pedina pedina = (Pedina)obj;
+            return this.Id==pedina.Id && this.Color==pedina.Color;
+        }
+    }
+}
