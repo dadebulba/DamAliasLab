71b58f7a4d27127dc130a2bdae5749ffeddbae58
Giaco131 <giaco131.ktm@gmail.com>
2020-11-11 13:49:19 +0100

    Allego la classe Coordinate #10

---
+++ back-end/VivaLaDama/VivaLaDama/Models/Coordinate.cs
@@ -0,0 +1,42 @@
+﻿namespace VivaLaDama.Models
+{
+    public class Coordinate
+    {
+        public int Riga { get; set; }
+        public int Colonna { get; set; }
+        public Coordinate(int riga, int colonna)
+        {
+            this.Riga = riga;
+            this.Colonna = colonna;
+        }
+        public override bool Equals(object obj)
+        {
+            if(obj==null || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            Coordinate c = (Coordinate)obj;
+            return this.Riga == c.Riga && this.Colonna == c.Colonna;
+        }
+        public Coordinate GetDownLeft()
+        {
+            return new Coordinate(Riga + 1, Colonna - 1);
+        }
+        public Coordinate GetDownRight()
+        {
+            return new Coordinate(Riga + 1, Colonna + 1);
+        }
+        public Coordinate GetUpLeft()
+        {
+            return new Coordinate(Riga - 1, Colonna - 1);
+        }
+        public Coordinate GetUpRight()
+        {
+            return new Coordinate(Riga - 1, Colonna + 1);
+        }
+        public bool IsValid(long maxValue)
+        {
+            return Riga >= 0 && Colonna >= 0 && Riga < maxValue && Colonna < maxValue;
+        }
+    }
+}
