5787c9e1d9669761105c26c038135f75aec5ef85
Giaco131 <giaco131.ktm@gmail.com>
2020-11-11 13:47:51 +0100

    Creazione della classe Mossa #14

---
+++ back-end/VivaLaDama/VivaLaDama/Models/Mossa.cs
@@ -0,0 +1,15 @@
+﻿namespace VivaLaDama.Models
+{
+    public class Mossa
+    {
+        public Pedina Target { get; set; }
+        public Coordinate From { get; set; }
+        public Coordinate To { get; set; }
+        public Mossa(Pedina target, Coordinate from, Coordinate to)
+        {
+            this.Target = target;
+            this.From = from;
+            this.To = to;
+        }
+    }
+}
