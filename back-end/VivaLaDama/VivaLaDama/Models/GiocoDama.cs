c1cae4c7c3f5cb0e864bbf0dd8d40369c89ec533
Giaco131 <giaco131.ktm@gmail.com>
2020-11-11 13:50:58 +0100

    Realizzazione classe per mantere lo stato di una partita #9

---
+++ back-end/VivaLaDama/VivaLaDama/Models/GiocoDama.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VivaLaDama.Models
+{
+    public class GiocoDama
+    {
+        public long IdPartita { get; set; }
+        public string NomePlayer1 { get; set; }
+        public string NomePlayer2 { get; set; }
+        public CampoDiGioco Scacchiera { get; set; }
+        public Mossa[] CronologiaMosseValide { get; set;  }
+    }
+}
