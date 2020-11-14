1e671fb28e0e1115130e26017b74e4a99b304622
Giaco131 <giaco131.ktm@gmail.com>
2020-11-14 18:06:47 +0100

    Ho tradotto il nome della classe e degli attributi in inglese #19

---
+++ back-end/VivaLaDama/VivaLaDama/Models/Move.cs
@@ -0,0 +1,15 @@
+﻿namespace VivaLaDama.Models
+{
+    public class Move
+    {
+        public Pawn Target { get; set; }//The target is the pawn that has been moved
+        public Coordinates From { get; set; }
+        public Coordinates To { get; set; }
+        public Move(Pawn target, Coordinates from, Coordinates to)
+        {
+            this.Target = target;
+            this.From = from;
+            this.To = to;
+        }
+    }
+}
