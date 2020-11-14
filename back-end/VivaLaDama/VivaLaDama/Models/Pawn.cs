f01cc06c8ca027d92663a7f8f4daee6129046bac
Giaco131 <giaco131.ktm@gmail.com>
2020-11-14 18:03:34 +0100

    Ho tradotto il nome degli attributi e della classe in inglese #20

---
+++ back-end/VivaLaDama/VivaLaDama/Models/Pawn.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VivaLaDama.Models
+{
+    public class Pawn
+    {
+        public enum ColorPawn { WHITE, BLACK }
+        public long Id { get; }
+        public ColorPawn Color { get; }
+        public bool Upgraded { get; set; }
+        public Pawn(ColorPawn color, long id)
+        {
+            this.Color = color;
+            this.Id = id;
+            this.Upgraded = false;
+        }
+        public ColorPawn GetColoreOpponente()
+        {
+            if(this.Color!=ColorPawn.WHITE && this.Color !=ColorPawn.BLACK)
+            {
+                return this.Color;
+            }
+            return (this.Color == ColorPawn.WHITE ? ColorPawn.BLACK : ColorPawn.WHITE);
+        }
+        public override bool Equals(Object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            Pawn pawn = (Pawn)obj;
+            return this.Id==pawn.Id && this.Color==pawn.Color;
+        }
+    }
+}
