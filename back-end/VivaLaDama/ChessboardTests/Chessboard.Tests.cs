7f9223055c2f7e48883ea11ed76f1cc23d879026
Giaco131 <giaco131.ktm@gmail.com>
2020-11-17 12:14:50 +0100

    Aggiunto test #16
    Ho controllato se una pedina nera è in grado di mangiare una pedina bianca e se effettivamente questa viene eliminata o meno.

--- back-end/VivaLaDama/ChessboardTests/Chessboard.Tests.cs
+++ back-end/VivaLaDama/ChessboardTests/Chessboard.Tests.cs
@@ -53,14 +53,14 @@ namespace VivaLaDama.UnitTests.Models
         }
         public static IEnumerable<object[]> GetData_ChessboardExecuteMove_CheckingMovementWhite()
         {
-            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 1), new Coordinate(4, 1)), true};
-            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 1), new Coordinate(4, 3)), true};
-            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 1), new Coordinate(6, 1)), false};
-            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 1), new Coordinate(6, 3)), false};
-            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 1), new Coordinate(4, 2)), false};
-            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 1), new Coordinate(3, 2)), false};
-            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 1), new Coordinate(3, 0)), false};
-            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 1), new Coordinate(3, 4)), false};
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 1), new Coordinate(4, 1)), true };
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 1), new Coordinate(4, 3)), true };
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 1), new Coordinate(6, 1)), false };
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 1), new Coordinate(6, 3)), false };
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 1), new Coordinate(4, 2)), false };
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 1), new Coordinate(3, 2)), false };
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 1), new Coordinate(3, 0)), false };
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 1), new Coordinate(3, 4)), false };
         }
         [DataTestMethod]
         [DynamicData(nameof(GetData_ChessboardExecuteMove_CheckingMovementWhiteOutOfGrid), DynamicDataSourceType.Method)]
@@ -78,8 +78,77 @@ namespace VivaLaDama.UnitTests.Models
         {
             yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 0), new Coordinate(4, -1))};
             yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 0), new Coordinate(5, -2))};
-            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 3), new Coordinate(4, 8))};
-            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 3), new Coordinate(5, 9))};
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 3), new Coordinate(5, 8))};
+        }
+        [DataTestMethod]
+        [DynamicData(nameof(GetData_ChessboardExecuteMove_CheckingMovementBlack), DynamicDataSourceType.Method)]
+        public void ChessboardExecuteMove_CheckingMovementBlack(Move move, bool resultExpected)
+        {
+            Chessboard chessboard = new Chessboard();
+            bool result;
+
+            chessboard.SetTurnForBlack();
+            result = chessboard.ExecuteMove(move);
+
+            Assert.AreEqual(resultExpected, result);
+        }
+        public static IEnumerable<object[]> GetData_ChessboardExecuteMove_CheckingMovementBlack()
+        {
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.BLACK, 9), new Coordinate(3, 2)), true};
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.BLACK, 9), new Coordinate(3, 4)), true};
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.BLACK, 9), new Coordinate(1, 2)), false};
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.BLACK, 9), new Coordinate(1, 4)), false};
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.BLACK, 9), new Coordinate(4, 1)), false};
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.BLACK, 9), new Coordinate(4, 5)), false};
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.BLACK, 9), new Coordinate(4, 3)), false};
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.BLACK, 9), new Coordinate(3, 3)), false};
+        }
+        [DataTestMethod]
+        [DynamicData(nameof(GetData_ChessboardExecuteMove_CheckingMovementBlackOutOfGrid), DynamicDataSourceType.Method)]
+        public void ChessboardExecuteMove_CheckingMovementBlackOutOfGrid_ReturnFalse(Move move)
+        {
+            Chessboard chessboard = new Chessboard();
+            bool result;
+
+            chessboard.SetTurnForWhite();
+            result = chessboard.ExecuteMove(move);
+
+            Assert.IsFalse(result, "A white pawn should not be able to go out of the grid!");
+        }
+        public static IEnumerable<object[]> GetData_ChessboardExecuteMove_CheckingMovementBlackOutOfGrid()
+        {
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 11), new Coordinate(4, -1))};
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 11), new Coordinate(5, -2))};
+            yield return new object[] { new Move(new Pawn(Pawn.ColorPawn.WHITE, 8), new Coordinate(4, -1))};
+        }
+        [TestMethod]
+        public void ChessboardExecute_CheckingIfBlackCanAttack_ReturnTrue()
+        {
+            Chessboard chessboard = new Chessboard();
+            Pawn blackPawn = new Pawn(Pawn.ColorPawn.BLACK, 8);
+            Pawn whitePawn = new Pawn(Pawn.ColorPawn.WHITE, 1);
+            Move moveBlack1 = new Move(blackPawn, new Coordinate(3, 0));
+            Move moveBlack2 = new Move(blackPawn, new Coordinate(5, 2));
+            Move moveWhite = new Move(whitePawn, new Coordinate(4, 1));
+            int numWhitePawns, numBlackPawns;
+            bool result;
+
+            result = chessboard.ExecuteMove(moveBlack1);
+            Assert.IsTrue(result, "The first black move should be valid!");
+
+            result = chessboard.ExecuteMove(moveWhite);
+            Assert.IsTrue(result, "The first white move should be valid!");
+
+            result = chessboard.ExecuteMove(moveBlack2);
+            Assert.IsTrue(result, "The second black move should be valid!");
+
+            numWhitePawns = chessboard.GetNumberOfPawnsOfColor(Pawn.ColorPawn.WHITE);
+            numBlackPawns = chessboard.GetNumberOfPawnsOfColor(Pawn.ColorPawn.BLACK);
+            result = numBlackPawns > numWhitePawns;
+            Assert.IsTrue(result, "The number of white pawns should be lower then the number of black pawns!");
+
+            result = chessboard.DoesThisPawnExist(whitePawn);
+            Assert.IsFalse(result, "This pawn should not exist anymore");
         }
     }
 }
