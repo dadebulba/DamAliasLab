2fe4ff2c56d0f36421784b5103441a1e51c531b9
Giaco131 <giaco131.ktm@gmail.com>
2020-11-22 13:27:23 +0100

    Aggiunta classe PawnPositioned #27
    Aggiunto il contesto di GameSession e la classe PawnPositioned

---
+++ back-end/VivaLaDama/VivaLaDama/Models/GameSessionContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VivaLaDama.Models
+{
+    public class GameSessionContext : DbContext
+    {
+        public GameSessionContext(DbContextOptions<GameSessionContext> options) : base(options)
+        {
+        }
+
+        public DbSet<GameSession> GameSessions { get; set; }
+    }
+}
---
+++ back-end/VivaLaDama/VivaLaDama/Models/PawnPositioned.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VivaLaDama.Models
+{
+    public class PawnPositioned : Pawn
+    {
+        public Coordinate Position { get; }
+        public PawnPositioned(ColorPawn color, long id, Coordinate position) : base(color, id)
+        {
+            this.Position = position;
+        }
+    }
+}
