c3a55173ef1c632c093dab168bd89903ec4e52ff
Federico Diprima <federicodiprima@Air-di-Federico.lan>
2020-11-25 00:02:57 +0100

    get/game request url fittizio #23

--- front-end/apiService.js
+++ front-end/apiService.js
@@ -1,21 +1,25 @@
-const URL = "https://postman-echo.com";
-const GET_ENDPOINT = "get";
-const POST_ENDPOINT = "post";
+//const URL = "https://jsonplaceholder.typicode.com/todos/1";
+//const GET_ENDPOINT = "get";
+//const POST_ENDPOINT = "post";
+const URL1 = "https://localhost:5001/weatherforecast";
+let form=document.getElementById('form');
 
-async function getGame(foo1, foo2) {
-    return fetch(`${URL}/${GET_ENDPOINT}?foo1=${foo1}&foo2=${foo2}`, {
+async function getGame() {
+    let response= await fetch(URL1, {
         method: 'GET'
     });
+    return response;
 }
 
-async function postGame(body) {
-    return fetch(`${URL}/${POST_ENDPOINT}`, {
+async function postGame() {
+    return fetch(URL, {
         method: 'POST',
-        headers: {
+        /*headers: {
             'Content-Type': 'application/json;charset=utf-8'
-        },
-        body: JSON.stringify(body)
-    });
+        },*/
+        body: new FormData(form)
+      });
 }
+
 
 export { getGame, postGame };
--- front-end/events.js
+++ front-end/events.js
@@ -1,47 +1,68 @@
-
-let elem=document.getElementById('resume');
-elem.addEventListener('click', prova);
-elem.addEventListener('click',viewGamesTable);
+import {getGame, postGame} from "./apiService.js";
 
 let j=0;
+let ID;
+let resumegameButton=document.getElementById('resume');
+let newgameButton=document.getElementById('new');
+let form=document.getElementById('form');
+let conteinerTab=document.getElementById('container-tab');
+let tableGames=document.getElementById('table-games');
+//let submitButton=document.getElementById("submit");
 
-async function viewGamesTable(){
-    tab=document.getElementById('container-tab');
-    tab.style.display='flex';
+
+resumegameButton.addEventListener('click', viewTableGames);
+newgameButton.addEventListener('click', viewForm );
+tableGames.addEventListener('click', gameChosen);
+form.addEventListener('submit', sendForm);
+
+
+function viewForm(){
+    form.style.display='flex'; //view form 
+    conteinerTab.style.display='none';  //hide table
 }
 
-async function prova(){
+async function viewTableGames(){
     if(j==0){
-        let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
-        let response = await fetch(url);
-        
-        let json = await response.json(); // read response body and parse as JSON
-        
-        
-        let nome=json[0].author.login;
-            
-        let tr = document.createElement('tr');
-        tr.className = "rows";
-    
-        tr.innerHTML=`<td>ID3</td> <td>${nome}-Player8</td> <td>10-50</td>`;
 
-        let table=document.getElementById("heading");
-        table.after(tr);
+        let response = await getGame();
+
+        let obj = await response.json(); // read response body and parse as JSON
+
+        //E' UNA PROVA
+        for(let key in obj){
+            let tr = document.createElement('tr');
+            tr.className = `rows`; //poi aggiungere class${key} se serve una classe diversa per ogni riga
+            tr.innerHTML=`<td> ${key} </td> <td>${obj[key].temperatureC}-${obj[key].temperatureF}</td> <td><button class="lastColumn" id="${key}" >play!</button></td>`;
+            let heading=document.getElementById("heading");
+            heading.after(tr);
+        }
         j++;
+    } 
+    conteinerTab.style.display='flex'; //view table 
+    form.style.display='none'; //hideForm
+}
+
+function gameChosen(){
+    let target = event.target;  // where was the click
+    if(target.className=="lastColumn"){  //only se click on play button
+        ID=target.id;
+        location.href="./damaMainPage.html";
+        //devo chiamare GET ID e andare nell'altra pagina
     }
 }
 
-/*
-function f(){
-    alert("bho");
-    let elem=event.target;
-    if(elem.tagName=="TD"){
-        alert(elem.id);
-        elem.innerHTML="<div class='piece black-piece'> </div>"
-    }
-    else {
-        alert("gia occupato");
-        elem.hidden=true;
-    }
+async function sendForm (evt){
+    
+    evt.preventDefault();
+
+    /*let player1 = document.getElementById("player1");
+    let player2 = document.getElementById("player2");
+    console.log(player1.value);
+    console.log(player2.value);
+    title.innerHTML = `${player1.value} and ${player2.value}`;*/
+    
+    let response = await postGame();
+    let result = await response.json(); 
+
+    alert(result.message);
 }
-*/
