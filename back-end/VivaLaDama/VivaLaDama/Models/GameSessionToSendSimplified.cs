4c0ce227eb684aa0a68b34897681ac6242451d89
Giaco131 <giaco131.ktm@gmail.com>
2020-11-25 12:27:18 +0100

    Aggiunta classe di supporto #27
    Aggiunta la classe da inviare quando si richiama l'api GET /game/

---
+++ back-end/VivaLaDama/VivaLaDama/Models/GameSessionToSendSimplified.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VivaLaDama.Models
+{
+    public class GameSessionToSendSimplified
+    {
+        public int Id { get; }
+        public string NamePlayer1 { get; }
+        public string NamePlayer2 { get; }
+        public GameSessionToSendSimplified(int id, string namePlayer1, string namePlayer2)
+        {
+            this.Id = id;
+            this.NamePlayer1 = namePlayer1;
+            this.NamePlayer2 = namePlayer2;
+        }
+    }
+}
