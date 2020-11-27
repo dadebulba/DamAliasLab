8dd727ce12e25b08884382d3ebf15fbb06fd335f
Giaco131 <giaco131.ktm@gmail.com>
2020-11-27 12:48:09 +0100

    Metodo PUT non funzionante #5
    Non legge i valori in ingresso

--- back-end/VivaLaDama/VivaLaDama/Controllers/GameSessionController.cs
+++ back-end/VivaLaDama/VivaLaDama/Controllers/GameSessionController.cs
@@ -27,21 +27,22 @@ namespace VivaLaDama.Controllers
         {
             return await _context.GameSessions.Select(x => new GameSessionToSendSimplified(x)).ToListAsync();
         }
+        //GET api/game/131
         [HttpGet("{id}")]
-        public async Task<ActionResult<GameSessionToSendSimplified>> GetGameSession(long id)
+        public async Task<ActionResult<GameSessionToSend>> GetGameSession(long id)
         {
-            GameSession game = await this._context.GameSessions.FindAsync(id);
+            GameSession game = (await this.RetrieveGameSession(id)).Value;
 
-            if(game==null)
+            if (game == null)
             {
                 return NotFound();
             }
 
-            return new GameSessionToSendSimplified(game);
+            return new GameSessionToSend(game);
         }
         //POST api/game
         [HttpPost]
-        public async Task<ActionResult<IEnumerable<NameOfPlayers>>> PostGameSessions(NameOfPlayers names)
+        public async Task<ActionResult<IEnumerable<GameSessionToSend>>> PostGameSessions(NameOfPlayers names)
         {
             GameSession game = new GameSession();
             game.NamePlayer1 = names.NamePlayer1;
@@ -51,6 +52,56 @@ namespace VivaLaDama.Controllers
             await this._context.SaveChangesAsync();
 
             return CreatedAtAction(nameof(GetGameSessions), new { id = game.IdGame }, new GameSessionToSend(game));
+        }
+        //PUT api/game/131
+        [HttpPut("{id}")]
+        public async Task<ActionResult<GameSessionToSend>> PutGameSession(long id, MoveToRecv move)
+        {
+            GameSession game = (await this.RetrieveGameSession(id)).Value;
+
+            if(game==null)
+            {
+                return NotFound();
+            }
+
+            Console.WriteLine("--------------------PUT--------------------");
+            Console.WriteLine("move.Target = idTarget:{0}, colorTarget:{1}", move.Target.Id, move.Target.Color);
+            Console.WriteLine("move.To = ({0},{1})", move.To.Row, move.To.Column);
+
+            if (/*game.ExecuteMove(move)*/false)
+            {
+                try
+                {
+                    await this._context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!this.GameSessionExists(id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+            else
+            {
+                return StatusCode(403);
+            }
+
+            return new GameSessionToSend(game);
+        }
+        private bool GameSessionExists(long id)
+        {
+            return this._context.GameSessions.Any(game => game.IdGame == id);
+        }
+        private async Task<ActionResult<GameSession>> RetrieveGameSession(long id)
+        {
+            GameSession game = await this._context.GameSessions.FindAsync(id);
+
+            return game;
         }
     }
 }
