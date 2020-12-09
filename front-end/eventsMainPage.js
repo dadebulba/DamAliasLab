3dd155b03fb816c34f298bb6eef07efde256987b
Federico Diprima <federicodiprima@Air-di-Federico.lan>
2020-12-09 17:54:06 +0100

    integrato put con backend #52

--- front-end/eventsMainPage.js
+++ front-end/eventsMainPage.js
@@ -3,7 +3,6 @@ import {getID, put} from "./apiService.js";
 let nameP1=document.getElementById("name-player1");
 let nameP2=document.getElementById("name-player2");
 let chessboard=document.getElementById("chessboard");
-let selected=false;
 let id=0; 
 
 let urlParam = new URLSearchParams(window.location.search);
@@ -19,67 +18,62 @@ chessboard.addEventListener("click", selectPawn);
 
 function selectPawn() { 
   let target = event.target;
-  if (target.classList.contains("piece")) {
+  if (target.tagName == "TD") {
+    alert("Seleziona una pedina!");
+  }
+  else {
     id = target.id;
     target.parentElement.style.backgroundColor = "red";
     chessboard.removeEventListener("click", selectPawn);
     chessboard.addEventListener("click", selectDest);
   }
-  if (target.tagName == "TD") {
-    alert("Devi selezionare una pedina")
-  }
 }
 
 async function selectDest() {
   let target = event.target;
+  let dest;
   if(target.classList.contains("piece")){
-    alert("Mossa non valida");
-    //annulla selezione
-    document.getElementById(id).parentElement.style.backgroundColor = "#8997a9";
-    chessboard.addEventListener("click", selectPawn);
-    chessboard.removeEventListener("click", selectDest);
+    dest = target.parentElement.id;
   }
   if (target.tagName == "TD") {
-    let dest = target.id;
-    document.getElementById(id).parentElement.style.backgroundColor = "#8997a9";
-
+    dest = target.id;
+  }
+  document.getElementById(id).parentElement.style.backgroundColor = "#8997a9";
     let obj = {
       target: {
-        id: id.substring(1,),
+        pawnId: Number(id.substring(1,)),
         color: id[0]=="w" ? 0 : 1,  //da chiedere cosa significa 0 e 1 (bianco nero)
       },
       to: {
-        row: dest[1],
-        column: dest[2]
+        row: Number(dest[1]),
+        column: Number(dest[2])
       }
     }
-
-    let response = await put(IDpartita, obj);
-    //let result = await response.json();
-    /*
-      SE LA MOSSA NON è VALIDA GESTISCO L'ERRORE
-      ----> SE LA MOSSA è VALIDA MI TORNA OGGETTO PARTITA, (MOCKATA)
-    */
-    movePawn(response);
-    updateMoves(response.moves);//lo faccio meglio quando so struttura definitiva mosse
-    insertPoints(response.pointsWhite, response.pointsBlack);
-    viewTurn(response.turn);
-
     
+    let response = await put(IDpartita, obj);
+
+    if(response!=null){
+      let result = await response.json();
+      movePawn(result);
+      updateMoves(result.moves);//lo faccio meglio quando so struttura definitiva mosse
+      insertPoints(result.pointsWhite, result.pointsBlack);
+      viewTurn(result.turn);
+    }
+
     chessboard.addEventListener("click", selectPawn);
     chessboard.removeEventListener("click", selectDest);
-  }
 }
 
 function updateMoves(moves){
   let li = document.createElement('li');
   let length=moves.length;
-  li.innerHTML=`${moves[length-1]}`;
+  li.innerHTML=`${moves[length-1].target.color==1 ? "BLACK" : "WHITE"}
+  from: [${moves[length-1].from.row},${moves[length-1].from.column}]
+  to: [${moves[length-1].to.row},${moves[length-1].to.column}]`;
   document.getElementById("moves-list").prepend(li);
 
 }
 function movePawn(partita){
-  //document.getElementById(`${move.pawn.color=="WHITE" ? "w" : "b"}${move.pawn.id}`).parentElement.innerHTML="";
   clearChessboard();
   insertBlackPawns(partita.black);
   insertWhitePawns(partita.white);
@@ -89,28 +83,27 @@ async function initializeGame(){
   let response = await getID(IDpartita);
   let result = await response.json();  //oggetto partita  
   insertPlayerNames(result.namePlayer1, result.namePlayer2);
-  insertMoves(result.moves); //da modificare quando so la struttura
+  insertMoves(result.moves);
   insertBlackPawns(result.black);
   insertWhitePawns(result.white);
-  insertPoints(result.pointsWhite, result.pointsBlack);
+  insertPoints(result.pointsBlack, result.pointsWhite);
   viewTurn(result.turn);
 }
-  
+
 function viewTurn(turn){
-  if(turn==1){
+  if(turn==0){
     document.getElementById("turn-box").innerHTML=`TOCCA A: <div id="turn-label" class='white-label'> </div>`;
   }
   else{
     document.getElementById("turn-box").innerHTML=`TOCCA A: <div id="turn-label" class='black-label'> </div>`;
   }
-
 }
 function insertPoints(black, white){
-  document.getElementById("black-points").innerHTML= `&nbsp&nbsp${black}`;
-  document.getElementById("white-points").innerHTML= `&nbsp&nbsp${white}`;
+  document.getElementById("black-points").innerHTML= `&nbsp&nbsp${black*25}`;
+  document.getElementById("white-points").innerHTML= `&nbsp&nbsp${white*25}`;
 }
 function insertPlayerNames(namePlayer1, namePlayer2){
-    nameP1.innerHTML=`<div class='label black-label'> </div> ${namePlayer1}: <div id="black-points"> </div>`;
+    nameP1.innerHTML=`<div class='label black-label'> </div>${namePlayer1}: <div id="black-points"> </div>`;
     nameP1.style.fontSize="20px";
     nameP2.innerHTML=`<div class='label white-label'> </div> ${namePlayer2}: <div id="white-points"> </div>`;
     nameP2.style.fontSize="20px";
@@ -119,7 +112,9 @@ function insertPlayerNames(namePlayer1, namePlayer2){
 function insertMoves(movesArray){
   for(let key in movesArray){
     let li = document.createElement('li');
-    li.innerHTML=`${movesArray[key]}`;
+    li.innerHTML=`${movesArray[key].target.color==1 ? "BLACK" : "WHITE"}
+    from: [${movesArray[key].from.row},${movesArray[key].from.column}]
+    to: [${movesArray[key].to.row},${movesArray[key].to.column}]`;
     document.getElementById("moves-list").prepend(li);
   }
 }
@@ -133,7 +128,7 @@ function insertBlackPawns(black) {
       elem.innerHTML = `<div class='piece black-piece' id="b${black[key].pawnId}"> </div>`;
     }
     else{ 
-      elem.innerHTML=`<div class='piece black-upgraded' id="b${black[key].pawnId}"> </div>`;
+      elem.innerHTML=`<div class='piece black-piece upgraded' style="color:red" id="b${black[key].pawnId}"> &#x265b </div>`;
     }
   }
 }
@@ -146,7 +141,7 @@ function insertWhitePawns(white) {
       elem.innerHTML = `<div class='piece white-piece' id="w${white[key].pawnId}"> </div>`;
     }
     else{ 
-      elem.innerHTML=`<div class='piece white-upgraded' id="w${white[key].pawnId}"> </div>`;
+      elem.innerHTML=`<div class='piece white-piece upgraded' style="color:red"  id="w${white[key].pawnId}"> &#x265b </div>`;
     }
   }
 }
@@ -160,35 +155,3 @@ function clearChessboard(){
     }
   }
 }
-
-/*
-function movePawn(move, partita){
-  if(move.pawn.color=="WHITE"){
-    for(let key in partita.white){
-      if (partita.white[key].pawnId==move.pawn.id){
-        document.getElementById(`w${move.pawn.id}`).parentElement.innerHTML="";
-        let dest=document.getElementById(`a${partita.white[key].position.row}${partita.white[key].position.column}`);
-        if(partita.white[key].upgraded==false){
-          dest.innerHTML=`<div class='piece white-piece' id="w${partita.white[key].pawnId}"> </div>`;
-        }
-        else{
-          dest.innerHTML=`<div class='piece white-upgraded' id="w${partita.white[key].pawnId}"> </div>`; 
-        }
-      }
-    }
-  }
-  if(move.pawn.color=="BLACK"){
-    for(let key in partita.black){
-      if (partita.black[key].pawnId==move.pawn.id){
-        document.getElementById(`b${move.pawn.id}`).parentElement.innerHTML="";
-        let dest=document.getElementById(`a${partita.black[key].position.row}${partita.black[key].position.column}`);
-        if(partita.black[key].upgraded==false){
-          dest.innerHTML=`<div class='piece black-piece' id="b${partita.black[key].pawnId}"> </div>`;
-        }
-        else{
-          dest.innerHTML=`<div class='piece black-upgraded' id="b${partita.black[key].pawnId}"> O </div>`;  
-        }
-      }
-    }
-  }
-}*/
